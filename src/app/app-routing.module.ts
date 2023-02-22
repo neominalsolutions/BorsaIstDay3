855cdefdd0fb107a21a42f49887d30eda31f2758
mert.alptekin <mert.alptekin@neomina.com>
2023-02-22 12:33:16 +0300

    Angular Routing, Nested Routes, Layout Kavramı, router-outlet, routerLink directive

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,61 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminLayoutComponent } from './admin/layout/admin-layout/admin-layout.component';
+import { RolesPageComponent } from './admin/pages/roles-page/roles-page.component';
+import { UsersPageComponent } from './admin/pages/users-page/users-page.component';
+import { ClientLayoutComponent } from './client-ui/layout/components/client-layout/client-layout.component';
+import { AboutPageComponent } from './client-ui/pages/about-page/about-page.component';
+import { HomePageComponent } from './client-ui/pages/home-page/home-page.component';
 
-const routes: Routes = [];
+// route config dosyası
+const routes: Routes = [
+  // ayrı bir module linki
+  {
+    path: '',
+    component: ClientLayoutComponent,
+    children: [
+      // nested route
+      {
+        path: '',
+        component: HomePageComponent,
+      },
+      {
+        path: 'home',
+        component: HomePageComponent,
+      },
+      {
+        path: 'about',
+        component: AboutPageComponent,
+      },
+    ],
+  },
+  // ayrı bir module layout linki
+  {
+    path: 'admin',
+    component: AdminLayoutComponent,
+    children: [
+      {
+        path: 'users',
+        component: UsersPageComponent,
+      },
+      {
+        path: 'roles',
+        component: RolesPageComponent,
+      },
+    ],
+  },
+  // {
+  //   path: 'home',
+  //   component: HomePageComponent,
+  // },
+  // {
+  //   path: 'about',
+  //   component: AboutPageComponent,
+  // },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutPageComponent } from './client-ui/pages/about-page/about-page.component';
+import { HomePageComponent } from './client-ui/pages/home-page/home-page.component';
+import { HeaderComponent } from './client-ui/layout/components/header/header.component';
+import { FooterComponent } from './client-ui/layout/components/footer/footer.component';
+import { MenuComponent } from './client-ui/layout/components/menu/menu.component';
+import { ClientLayoutComponent } from './client-ui/layout/components/client-layout/client-layout.component';
+import { AdminLayoutComponent } from './admin/layout/admin-layout/admin-layout.component';
+import { UsersPageComponent } from './admin/pages/users-page/users-page.component';
+import { RolesPageComponent } from './admin/pages/roles-page/roles-page.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, HomePageComponent, AboutPageComponent, HeaderComponent, FooterComponent, MenuComponent, ClientLayoutComponent, AdminLayoutComponent, UsersPageComponent, RolesPageComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
